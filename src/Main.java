70d35f7cddc3156551246e9bba78fd279e8128db
rajkaran <rajkaran1505@gmail.com>
2025-10-23 23:26:22 +0530

    Commit Done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList();
         q.add(10); q.add(20); q.add(30); q.add(40);
 
         System.out.println("Original Queue: " + q);
